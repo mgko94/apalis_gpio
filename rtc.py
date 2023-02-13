4d99daccb86563299997c39177936b3132e8d4f0
mgko <m1373058422@gmail.com>
2023-02-13 13:13:38 +0900

    rtc edit

--- rtc.py
+++ rtc.py
@@ -6,10 +6,10 @@ RTC_RD_TIME=0x80247009
 
 # Identify RTCs
 rtcList = glob.glob('/dev/rtc[0-9]')
-print "RTCs: ", rtcList, "\n"
+print ("RTCs: ", rtcList, "\n")
 
 # Read each RTC
-for rtc in rtcList
+for rtc in rtcList :
     #  struct rtc_time {
     #    int tm_sec;
     #    int tm_min;
@@ -28,5 +28,5 @@ for rtc in rtcList
     input=fcntl.ioctl(fo.fileno(), RTC_RD_TIME, a)
     result=struct.unpack('iiiiiiiii', input)
 
-    print rtc + ": " + str(1900+result[5]) + "-" + str(1+result[4]) + "-" + str(result[3]) + " " + \
-          str(result[2]) + ":" + str(result[1]) + ":" + str(result[0]) + " UTC"
+    print (rtc + ": " + str(1900+result[5]) + "-" + str(1+result[4]) + "-" + str(result[3]) + " " + \
+          str(result[2]) + ":" + str(result[1]) + ":" + str(result[0]) + " UTC")
