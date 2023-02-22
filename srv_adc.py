4b8e069fceada1f0e7a5398c68f3324f1fb5d51b
John Doe <johndoe@example.com>
2023-02-22 21:14:32 +0900

    프로토콜 추가

---
+++ srv_adc.py
@@ -0,0 +1,49 @@
+import paho.mqtt.client as mqtt
+import json
+import os
+import time
+
+
+
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("connected OK")
+    else:
+        print("Bad connection Returned code=", rc)
+
+
+def on_disconnect(client, userdata, flags, rc=0):
+    print(str(rc))
+
+
+def on_publish(client, userdata, mid):
+    print("In on_pub callback mid= ", mid)
+
+
+# 새로운 클라이언트 생성
+client = mqtt.Client()
+# 콜백 함수 설정 on_connect(브로커에 접속), on_disconnect(브로커에 접속중료), on_publish(메세지 발행)
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_publish = on_publish
+# address : localhost, port: 1883 에 연결
+client.connect('broker.mqttdashboard.com', 1883)
+client.loop_start()
+# common topic 으로 메세지 발행
+
+
+client.publish('mgko', json.dumps({"CHANNEL": [1] , "STATUS": "01" , "DATA": "START"  }), 1)
+while True :    
+    try :
+        adc = os.popen('cat /dev/apalis-adc0').read()[:-1]
+        client.publish('mgko', json.dumps({"CHANNEL": "1" , "STATUS": "02" , "DATA": [int(adc)]  }), 1)
+        time.sleep(1)
+    except KeyboardInterrupt:
+        break
+    
+client.publish('mgko', json.dumps({"CHANNEL": [1] , "STATUS": "03" , "DATA": "STOP"  }), 1)
+client.loop_stop()
+# 연결 종료
+client.disconnect()
